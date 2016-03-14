cd01433305b197c522631709d54741c685a1def1
muzzynine <muzzynine@bigfrogs.co.kr>
2016-03-14 15:08:34 +0900

    Init commit

---
+++ elpa/dash-20151216.1315/dash-autoloads.el
@@ -0,0 +1,15 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("dash.el") (22208 8112 401614 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-autoloads.el ends here
---
+++ init.el
@@ -0,0 +1,87 @@
+;;---------------------------
+;; MELPA setup
+;;---------------------------
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   '("melpa" . "http://melpa.org/packages/")
+   t)
+  (package-initialize))
+
+;;---------------------------
+;; exec-path-from-shell
+;;---------------------------
+(when (memq window-system '(mac ns))
+        (exec-path-from-shell-initialize))
+
+;;---------------------------
+;; Auto-complete setup(plain)
+;;---------------------------
+(ac-config-default)
+
+;;---------------------------
+;; Auto-complete setup(go-autocomplete)
+;;---------------------------
+(require 'go-autocomplete)
+(require 'auto-complete-config)
+
+;;---------------------------
+;; Go syntax err check(goflymake
+;;---------------------------
+(add-to-list 'load-path "~/Workspace/go/src/github.com/dougm/goflymake")
+(require 'go-flymake)
+(require 'go-flycheck)
+
+;;--------------------------
+;; js auto-complete setup(tern)
+;;--------------------------
+(add-hook 'js-mode-hook (lambda () (tern-mode t)))
+(eval-after-load 'tern
+  '(progn
+     (require 'tern-auto-complete)
+     (tern-ac-setup)))
+
+;;---------------------------
+;; markdown edit setup
+;;---------------------------
+
+;;---------------------------
+;; org-mode setup
+;;---------------------------
+(require 'org)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+
+(setq org-agenda-files (list "~/Workspace/org/agenda"))
+
+;;---------------------------
+;; theme setup
+;;---------------------------
+(setf inhibit-splash-screen t)
+(switch-to-buffer (get-buffer-create "empty"))
+(delete-other-windows)
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'monokai t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("2da65cb7074c176ca0a33f06bcc83ef692c9175e41b6370f5e94eb5811d6ee3a" default)))
+ '(exec-path
+   (quote
+    ("/Library/Frameworks/Python.framework/Versions/3.4/bin/" "/usr/local/bin/" "/usr/local/sbin/" "/usr/bin/" "/bin/" "/usr/sbin/" "/sbin/" "/opt/X11/bin/" "/usr/local/git/bin/" "/usr/local/go/bin/" "/Applications/Emacs.app/Contents/MacOS/bin-x86_64-10_9/" "/Applications/Emacs.app/Contents/MacOS/libexec-x86_64-10_9/" "/Users/impyeong-gang/.yadr/bin/" "/Users/impyeong-gang/.yadr/bin/yadr/" "/Applications/Emacs.app/Contents/MacOS/libexec/" "/Users/impyeong-gang/Workspace/go/bin")))
+ '(send-mail-function (quote smtpmail-send-it))
+ '(smtpmail-smtp-server "smtp.gmail.com")
+ '(smtpmail-smtp-service 25))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
